9e113410ed8a01067bdfe8fada148b2afd1bb395
diegolauar <diego_pktd@hotmail.com>
2022-06-14 20:44:57 -0300

    DELETE Preium

--- src/repositories/premium-repository.js
+++ src/repositories/premium-repository.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 const Premium = mongoose.model('Premium')
 
 
-exports.getEstablishmentId = async(data) => {
-    const res = await Premium.find({establishmentId: data.headers.establishmentid});
+exports.getEstablishmentId = async (data) => {
+    const res = await Premium.find({ establishmentId: data.headers.establishmentid });
     return res;
 }
 
@@ -11,13 +11,11 @@ exports.getEstablishmentId = async(data) => {
 exports.create = async (data) => {
     data.body['establishmentId'] = data.headers.establishmentid
     var premium = new Premium(data.body)
-    await premium.save()    
+    await premium.save()
 }
 
 exports.update = async (data) => {
-    const res = await Premium.findOneAndUpdate([
-        {id: data.body._id},
-        {establishmentId: data.headers.establishmentid}], {
+    const res = await Premium.findByIdAndUpdate(data.body._id, {
         $set: {
             name: data.body.name,
             value: data.body.value
@@ -28,6 +26,5 @@ exports.update = async (data) => {
 }
 
 exports.delete = async (data) => {
-    await Premium.findOneAndRemove({id: data.body.id}, 
-        {establishmentId: data.headers.establishmentid})
+    await Premium.findOneAndRemove(data.body._id)
 }
